444ae5136274bfdd835aa80f5d28848edd602717
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-05-15 20:21:02 +0800

    chore: update home styles

--- apps/web-yardbird/src/features/home/components/home.component.ts
+++ apps/web-yardbird/src/features/home/components/home.component.ts
@@ -5,5 +5,6 @@ import { BaseComponent } from '@yardbird/xplat/core';
 @Component({
   selector: 'yardbird-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent extends BaseComponent {}
