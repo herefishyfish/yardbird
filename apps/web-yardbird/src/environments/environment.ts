e5ab7bf0d031eefc925068f1602bcfb289991f38
Dylan Llewellyn <46717769+herefishyfish@users.noreply.github.com>
2023-05-15 21:23:15 +0800

    fix: add envs

---
+++ apps/web-yardbird/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'yardbird-application',
+    appId: '1:964082585160:web:9451b6ca5043dcbf2e127d',
+    storageBucket: 'yardbird-application.appspot.com',
+    apiKey: 'AIzaSyCBDwDOq6WATclbyCTVM7m0V2Tp2xR5FBw',
+    authDomain: 'yardbird-application.firebaseapp.com',
+    messagingSenderId: '964082585160',
+    measurementId: 'G-F1E0ZM3GYQ',
+  },
+  production: false,
+  useEmulators: false,
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
